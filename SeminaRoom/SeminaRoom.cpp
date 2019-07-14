aef77a6e27864cc806636576732a879e82cfe41b
sturtle333 <sturtle333@gmail.com>
2019-07-14 23:00:51 +0900

    add SeminaRoom

---
+++ SeminaRoom/SeminaRoom.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int N;
+	cin>>N;
+	
+	int time[N][2];
+	for(int a=0;a<N;a++) {
+		cin>>time[a][0]>>time[a][1];
+	}
+	
+	int temp[N];
+	int temp2;
+	int result_[N];
+	for(int a=0;a<N;a++) {
+		
+	}
+	
+	
+	
+	return 0;
+}
